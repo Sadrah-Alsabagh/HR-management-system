2407659e3701c11de8c5b5685c8ea0faf257eadc
Sadrah Alsabagh <sedrasabagh33@gmail.com>
2023-05-25 16:24:23 +0300

    some edits

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 "use strict";
+
+//Previous lab 08
 let id = 1000; // Starting employee ID
 
 //1. display the employees using a literal object
 const allEmployees = [
     {
-        id: generateID(),
+        generateID: function() {
+            id++;
+            console.log(id);
+            return id;
+        },
         fullName: 'Ghazi Samer',
         department: 'Administration',
         level: 'Senior',
@@ -14,7 +20,11 @@ const allEmployees = [
         },
     },
     {
-        id: generateID(),
+        generateID: function() {
+            id++;
+            console.log(id);
+            return id;
+        },
         fullName: 'Lana Ali',
         department: 'Finance',
         level: 'Senior',
@@ -24,7 +34,11 @@ const allEmployees = [
         },
     },
     {
-        id: generateID(),
+        generateID: function() {
+            id++;
+            console.log(id);
+            return id;
+        },
         fullName: 'Tamara Ayoub',
         department: 'Marketing',
         level: 'Senior',
@@ -34,7 +48,11 @@ const allEmployees = [
         },
     },
     {
-        id: generateID(),
+        generateID: function() {
+            id++;
+            console.log(id);
+            return id;
+        },
         fullName: 'Safi Walid',
         department: 'Administration',
         level: 'Mid-Senior',
@@ -44,7 +62,11 @@ const allEmployees = [
         },
     },
     {
-        id: generateID(),
+        generateID: function() {
+            id++;
+            console.log(id);
+            return id;
+        },
         fullName: 'Omar Zaid',
         department: 'Development',
         level: 'Senior',
@@ -54,7 +76,11 @@ const allEmployees = [
         },
     },
     {
-        id: generateID(),
+        generateID: function() {
+            id++;
+            console.log(id);
+            return id;
+        },
         fullName: 'Rana Saleh',
         department: 'Development',
         level: 'Junior',
@@ -64,7 +90,11 @@ const allEmployees = [
         },
     },
     {
-        id: generateID(),
+        generateID: function() {
+            id++;
+            console.log(id);
+            return id;
+        },
         fullName: 'Hadi Ahmad',
         department: 'Finance',
         level: 'Mid-Senior',
@@ -75,14 +105,10 @@ const allEmployees = [
     }
 ];
 
-
-
-//2.Create a method to generate a unique four digits employee id number.
-function generateID() {
-    id++;
-    console.log(id);
-    return id;
-}
+//Print the IDs
+allEmployees.forEach(function(employee) {
+    employee.generateID();
+});
 
 
 //3+4.Calculating salary.
@@ -116,3 +142,66 @@ function renderEmployees() {
   }
   
 renderEmployees();
+
+
+
+
+
+
+// Lab 09
+
+// let id = 1000;
+// const allEmployees = [];
+
+// function Employee(fullName, department, level, imageUrl) {
+//     this.id = function(){
+//          id++;
+//         console.log(id);
+//         return id;
+//     };
+//     this.fullName = fullName;
+//     this.department = department;
+//     this.level = level;
+//     this.imageUrl = imageUrl;
+//     this.salary = function calculateSalary(level) {
+//         const levelSalary = {
+//             "Senior": { min: 1500, max: 2000 },
+//             "Mid-Senior": { min: 1000, max: 1500 },
+//             "Junior": { min: 500, max: 1000 }
+//         };
+    
+//         const min = levelSalary[level].min;
+//         const max = levelSalary[level].max;
+    
+//         // Calculate random salary 
+//         const randomSalary = Math.floor(min + Math.random() * (max - min + 1));
+    
+//         const netSalary = randomSalary - (randomSalary * 0.075);
+//         return netSalary;
+//     };
+    
+//     allEmployees.push(this);
+// }
+
+// // 5.Render employees
+// Employee.prototype.renderEmployees = function () {
+//     document.write(`<p>Employee name: ${this.fullName}<br>Department: ${this.department}<br>Employee salary: ${this.salary}</p><br><br>`);
+//     };
+
+// let gazi = new Employee("Ghazi Samer", "Administration", "Senior", "https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+// let lana = new Employee("Lana Ali", "Finance", "Senior", "https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+// let tamara = new Employee("Tamara Ayoub", "Marketing", "Senior", "https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+// let safi = new Employee("Safi Walid", "Administration", "Mid-Senior", "https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+// let omar = new Employee("Omar Ziad", "Development", "Senior", "https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+// let rana = new Employee("Rana Saleh", "Development", "Junior", "https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+// let hadi = new Employee("Hadi Ahmad", "Finance", "Mid-Senior", "https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+
+// for (let i = 0; i < allEmployees.length; i++){
+//     allEmployees[i].renderEmployees();
+// }
+
+// //3+4.Calculating salary.
+
+
+
+
